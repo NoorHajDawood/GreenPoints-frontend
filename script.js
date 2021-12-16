398cd715437766b5a87125f1ccf6a309c0176723
NoorHajDawood <noorhajdawood@gmail.com>
2021-12-16 11:45:03 +0200

    alert on geo fail

--- script.js
+++ script.js
@@ -185,13 +185,16 @@ function initMap() {
 }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-    infoWindow.setPosition(pos);
-    infoWindow.setContent(
-        browserHasGeolocation
-            ? "Error: The Geolocation service failed."
-            : "Error: Your browser doesn't support geolocation."
-    );
-    infoWindow.open(map);
+    // infoWindow.setPosition(pos);
+    // infoWindow.setContent(
+    //     browserHasGeolocation
+    //         ? "Error: The Geolocation service failed."
+    //         : "Error: Your browser doesn't support geolocation."
+    // );
+    // infoWindow.open(map);
+    alert( browserHasGeolocation
+        ? "Error: The Geolocation service failed."
+        : "Error: Your browser doesn't support geolocation.");
 }
 
 /**
